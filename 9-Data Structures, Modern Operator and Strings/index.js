cf8900803cb0cd65a28f69a3fd08b856948114f2
Kakarot <105483542+caRot204@users.noreply.github.com>
2023-02-03 20:30:03 +0700

    update  destructuring Arr

--- 9-Data Structures, Modern Operator and Strings/index.js
+++ 9-Data Structures, Modern Operator and Strings/index.js
@@ -5,7 +5,11 @@ const restaurant = {
     location: 'Italy',
     categories: ['Italian ', 'Pizzeria', 'Vegetarian', 'Organic'],
     startMenu: ['Focaccia ', 'Bruschetta', 'Garlic Bred', 'Caprese Salad'],
-    mainMenu: ['Pizza', 'Pasta', 'Risotto']
+    mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+    order: function (starterIndex, mainIndex) {
+        return [this.startMenu[starterIndex], this.mainMenu[mainIndex]]
+    }
 }
 
 // Destructuring Arrays
@@ -25,3 +29,15 @@ console.log(a, b);
 // Switching Variables (chuyển đổi các biến);
 [a, b] = [b, a]
 console.log(a, b);
+const order1 = restaurant.order(2, 0);
+console.log(order1);
+
+const nested = [2, 3, [5, 6]]
+// const [i, , j] = nested
+const [i, , [j, k]] = nested
+console.log(i, j, k);
+
+//default values ( giá trị mặc định)
+
+const [p = 1, q = 1, r = 1] = [8, 9];
+console.log(p, q, r);
