119ec08181fbc563c3c4042368fc13723abb9eb8
Kakarot <105483542+caRot204@users.noreply.github.com>
2023-02-04 21:57:25 +0700

    lab2

--- lab2/index.js
+++ lab2/index.js
@@ -36,6 +36,14 @@ const btn = document.body.append(document.createElement('button'));
 
 document.querySelector('button').addEventListener('click', () => {
     const text = document.querySelector('textarea').value;
-    console.log(text);
+    const rows = text.split('\n');
+
+    console.log(rows);
+
+    for (const [i, row] of rows.entries()) {
+        const [first, second] = row.toLowerCase().trim().split('_');
+        const output = `${first}${second.replace(second[0], second[0].toUpperCase())}`;
+        console.log(`${output.padEnd(20)}${'✅'.repeat(i + 1)}`);
+    }
 })
 
